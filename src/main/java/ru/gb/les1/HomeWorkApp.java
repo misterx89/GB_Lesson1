60db24646231aa4073b8afc55d008b74a717eb5d
denisklassen <klassendv@mail.ru>
2021-08-06 16:54:58 +0500

    HW1

---
+++ src/main/java/ru/gb/les1/HomeWorkApp.java
@@ -0,0 +1,59 @@
+package ru.gb.les1;
+
+import java.net.SocketOption;
+
+public class HomeWorkApp {
+    public static void main(String[] args) {
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+    }
+
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+    public static void checkSumSign() {
+        int a = 10;
+        int b = -90;
+        int sum = a + b;
+
+        if (sum >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+
+    public static void printColor() {
+        int value = 120;
+
+        if (value <= 0) {
+            System.out.println("Красный");
+        } else if (value > 0 && value <= 100) {
+            System.out.println("Желтый");
+        } else if (value > 100) {
+            System.out.println("Зеленый");
+        }
+    }
+
+    public static void compareNumbers() {
+        int a=8;
+        int b=45;
+
+        if (a>=b){
+            System.out.println("a >= b");
+        }   else {
+            System.out.println("a < b");
+        }
+    }
+}
+
+
+
+
+
+
