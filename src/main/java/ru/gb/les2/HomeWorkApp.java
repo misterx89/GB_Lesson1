9411582f03abd0864f8a970532e5b1778a6acdad
denisklassen <klassendv@mail.ru>
2021-08-08 18:37:04 +0500

    HW1

--- src/main/java/ru/gb/les2/HomeWorkApp.java
+++ src/main/java/ru/gb/les2/HomeWorkApp.java
@@ -2,14 +2,17 @@ package ru.gb.les2;
 
 public class HomeWorkApp {
     public static void main(String[] args) {
-        System.out.println(checkSum(10,6));
+        System.out.println(checkSum(10, 6));
         checkNumber(9);
         System.out.println(checkReturnNumber(-50));
+        showString("Тест", 5);
+        //System.out.println(leapYear(2020));
+        leapYear(2004);
     }
 
     public static boolean checkSum(int a, int b) {
-        int sum = a+b;
-        if (sum>=10 && sum<=20) {
+        int sum = a + b;
+        if (sum >= 10 && sum <= 20) {
             System.out.println("sum = " + sum);
             return true;
         } else {
@@ -18,8 +21,8 @@ public class HomeWorkApp {
         }
     }
 
-    public static void checkNumber(int n){
-        if (n>=0){
+    public static void checkNumber(int n) {
+        if (n >= 0) {
             System.out.println("Положительное число");
         } else {
             System.out.println("Отрицательные число");
@@ -27,11 +30,36 @@ public class HomeWorkApp {
     }
 
     public static boolean checkReturnNumber(int n) {
-        if(n>=0){
+        if (n >= 0) {
             return false;
-        }   else{
+        } else {
             return true;
         }
     }
 
+    public static void showString(String s, int n) {
+        for (int i = 0; i < n; i++) {
+            System.out.println(s);
+        }
+    }
+
+    public static void leapYear(int thisYear) {
+        float step1 = thisYear % 4;
+        float step2 = thisYear % 100;
+        float step3 = thisYear % 400;
+
+        if (step1 != 0) {
+            System.out.println("step1 "+ step1);
+            //return false;
+        } else if (step2 != 0) {
+            System.out.println("step2 "+ step2);
+            //return false;
+        } else if (step3 != 0) {
+            System.out.println("step3 "+ step3);
+            //return false;
+        } else {
+            System.out.println("step3 "+ step3);
+            //return true;
+        }
+    }
 }
