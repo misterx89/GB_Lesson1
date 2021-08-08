fdce6afaa55105b1d30b82e0dc83b6bfa45d0b25
denisklassen <klassendv@mail.ru>
2021-08-08 15:18:06 +0500

    HW1

--- src/main/java/ru/gb/les2/HomeWorkApp.java
+++ src/main/java/ru/gb/les2/HomeWorkApp.java
@@ -3,8 +3,10 @@ package ru.gb.les2;
 public class HomeWorkApp {
     public static void main(String[] args) {
         System.out.println(checkSum(10,6));
-        checkNumber(-9);
+        checkNumber(9);
+        System.out.println(checkReturnNumber(-50));
     }
+
     public static boolean checkSum(int a, int b) {
         int sum = a+b;
         if (sum>=10 && sum<=20) {
@@ -15,11 +17,20 @@ public class HomeWorkApp {
             return false;
         }
     }
+
     public static void checkNumber(int n){
         if (n>=0){
             System.out.println("Положительное число");
         } else {
             System.out.println("Отрицательные число");
+        }
+    }
+
+    public static boolean checkReturnNumber(int n) {
+        if(n>=0){
+            return false;
+        }   else{
+            return true;
         }
     }
 
