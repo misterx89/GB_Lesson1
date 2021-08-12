3518ca64e2f3af8038565b97b479086d97f8fbe4
denisklassen <klassendv@mail.ru>
2021-08-12 23:32:02 +0500

    HW3

--- src/main/java/ru/gb/les3/HW.java
+++ src/main/java/ru/gb/les3/HW.java
@@ -4,14 +4,43 @@ import java.util.Arrays;
 
 public class HW {
     public static void main(String[] args) {
-        int arr[] = {1,0,1,1,0,0,1};
-        for (int i=0;i<arr.length;i++){
-            if (arr[i] == 0){
-                arr[i]=1;
-            }   else {
-                arr[i]=0;
+        System.out.println(Arrays.toString(Array1()));
+        System.out.println(Arrays.toString(Array2()));
+        System.out.println(Arrays.toString(Array3()));
+    }
+
+    public static int[] Array1() {
+        int arr[] = {1, 0, 1, 1, 0, 0, 1};
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 0) {
+                arr[i] = 1;
+            } else {
+                arr[i] = 0;
             }
         }
-        System.out.println(Arrays.toString(arr));
+        return arr;
+    }
+
+    public static int[] Array2() {
+        int arr[] = new int[100];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i]=i+1;
+        }
+        return arr;
+    }
+
+    public static int[] Array3() {
+        int arr[] = {1,5,3,2,11,4,5,2,4,8,9,1};
+        for(int i=0;i< arr.length;i++){
+            if(arr[i]<6){
+                arr[i]*=2;
+            }
+        }
+        return arr;
+    }
+
+    public static int[] Array4(){
+
     }
 }
+
