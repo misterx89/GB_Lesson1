913a85bd3901e73126c0385110d87f77cb09aee9
denisklassen <klassendv@mail.ru>
2021-08-13 12:25:13 +0500

    HW3

--- src/main/java/ru/gb/les3/HW.java
+++ src/main/java/ru/gb/les3/HW.java
@@ -7,6 +7,7 @@ public class HW {
         System.out.println(Arrays.toString(Array1()));
         System.out.println(Arrays.toString(Array2()));
         System.out.println(Arrays.toString(Array3()));
+        Array4();
     }
 
     public static int[] Array1() {
@@ -39,8 +40,19 @@ public class HW {
         return arr;
     }
 
-    public static int[] Array4(){
-
+    public static void Array4(){
+        int arr[][] = new int[5][5];
+        for(int i=0;i< arr.length;i++) {
+            for (int j = 0; j < arr.length; j++) {
+                        if (i+j== arr.length-1) {
+                            arr[i][j] = 1;
+                        } else if(i==j){
+                            arr[i][j] = 1;
+                        }
+                        System.out.print(arr[i][j] + " ");
+            }
+                    System.out.println();
+        }
     }
 }
 
