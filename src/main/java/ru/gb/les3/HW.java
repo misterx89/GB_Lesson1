15c9715aed7fb77c28a3d1be619470969c25c310
denisklassen <klassendv@mail.ru>
2021-08-14 15:13:34 +0500

    HW3

--- src/main/java/ru/gb/les3/HW.java
+++ src/main/java/ru/gb/les3/HW.java
@@ -1,17 +1,18 @@
 package ru.gb.les3;
 
+import javax.swing.*;
 import java.util.Arrays;
 
 public class HW {
     public static void main(String[] args) {
-        int[] arr = {2,2,2,1,2,2,10,1};
         System.out.println(Arrays.toString(Array1()));
         System.out.println(Arrays.toString(Array2()));
         System.out.println(Arrays.toString(Array3()));
         Array4();
         System.out.println(Arrays.toString(Array5(4, 57)));
         Array6();
-        System.out.println(Array7(arr));
+        System.out.println(Array7(new int[] {2,2,2,1,2,2,10,1}));
+        Array8(new int[] {5,9,7,3,6,88},-1);
     }
 
     public static int[] Array1() {
@@ -97,6 +98,35 @@ public class HW {
             return true;
            else
             return false;
+    }
+
+    public static void Array8(int arr[],int n) {
+        System.out.print("Do: ");
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+        System.out.println("(n = " + n + ")");
+        if (n > 0) {
+            for (int i = 0; i < n; i++) {
+                int tmp = arr[arr.length - 1];
+                for (int j = arr.length - 1; j > 0; j--) {
+                    arr[j] = arr[j - 1];
+                }
+                arr[0] = tmp;
+            }
+        } else if (n < 0) {
+            for (int i = 0; i < n * (-1); i++) {
+                int tmp = arr[0];
+                for (int j = 0; j < arr.length - 1; j++) {
+                    arr[j] = arr[j + 1];
+                }
+                arr[arr.length - 1] = tmp;
+            }
+        }
+        System.out.print("Posle:  ");
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
     }
 }
 
