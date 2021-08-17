2cd384a9dda7bedbdbf7cdbfaf16269637a62554
denisklassen <klassendv@mail.ru>
2021-08-17 08:20:51 +0600

    HW3

--- src/main/java/ru/gb/les3/HW.java
+++ src/main/java/ru/gb/les3/HW.java
@@ -5,17 +5,17 @@ import java.util.Arrays;
 
 public class HW {
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(Array1()));
-        System.out.println(Arrays.toString(Array2()));
-        System.out.println(Arrays.toString(Array3()));
-        Array4();
-        System.out.println(Arrays.toString(Array5(4, 57)));
-        Array6();
-        System.out.println(Array7(new int[] {2,2,2,1,2,2,10,1}));
-        Array8(new int[] {5,9,7,3,6,88},-1);
+        System.out.println(Arrays.toString(array1()));
+        System.out.println(Arrays.toString(array2()));
+        System.out.println(Arrays.toString(array3()));
+        array4();
+        System.out.println(Arrays.toString(array5(4, 57)));
+        array6();
+        System.out.println(array7(new int[] {2,2,2,1,2,2,10,1}));
+        array8(new int[] {5,9,7,3,6,88},-1);
     }
 
-    public static int[] Array1() {
+    public static int[] array1() {
         int[] arr = {1, 0, 1, 1, 0, 0, 1};
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == 0) {
@@ -27,7 +27,7 @@ public class HW {
         return arr;
     }
 
-    public static int[] Array2() {
+    public static int[] array2() {
         int[] arr = new int[100];
         for (int i = 0; i < arr.length; i++) {
             arr[i] = i + 1;
@@ -35,7 +35,7 @@ public class HW {
         return arr;
     }
 
-    public static int[] Array3() {
+    public static int[] array3() {
         int[] arr = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] < 6) {
@@ -45,7 +45,7 @@ public class HW {
         return arr;
     }
 
-    public static void Array4() {
+    public static void array4() {
         int[][] arr = new int[5][5];
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr.length; j++) {
@@ -60,7 +60,7 @@ public class HW {
         }
     }
 
-    public static int[] Array5(int len, int initialValue) {
+    public static int[] array5(int len, int initialValue) {
         int[] arr = new int[len];
         for (int i = 0; i < arr.length; i++) {
             arr[i] = initialValue;
@@ -68,39 +68,41 @@ public class HW {
         return arr;
     }
 
-    public static void Array6() {
+    public static void array6() {
 
         int[] arr = {1, 6, 9, 67, 345, 1, 8, 0};
         int max = 0;
         int min = 0;
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i] > max)
-                max = arr[i];
-            else
-                min = arr[i];
-        }
-        System.out.println(Arrays.toString(arr));
-        System.out.println("Max = " + max);
-        System.out.println("Min = " + min);
-    }
+            if (arr[i] > max) {
 
-    public static boolean Array7(int arr[]) {
+                max = arr[i];
+            } else {
+            }
+            min = arr[i];
+            System.out.println(Arrays.toString(arr));
+            System.out.println("Max = " + max);
+            System.out.println("Min = " + min);
+        }
+    }
+    public static boolean array7(int arr[]) {
 //        int[] arr = {2,2,2,1,2,2,10,1};
         int leftSum = 0;
         int rightSum = 0;
 
         for (int i = 0, j = 0;i < arr.length-j; i++) {
             leftSum += arr[i];
-            if (leftSum > rightSum)
+            if (leftSum > rightSum) {
                 rightSum += arr[arr.length - 1 - j++];
+            }
         }
-        if(leftSum==rightSum)
+        if(leftSum==rightSum) {
             return true;
-           else
-            return false;
+        }else {
+        }return false;
     }
 
-    public static void Array8(int arr[],int n) {
+    public static void array8(int arr[],int n) {
         System.out.print("Do: ");
         for (int i : arr) {
             System.out.print(i + " ");
